abb79373682b2cd32de5e5dcdab196f9b98fb6ee
VirginiaIvanova <113673043+Virginia1188@users.noreply.github.com>
2022-10-13 13:23:30 +0300

    Delete 1.7. listOfProducts.js

--- 1.7. listOfProducts.js
+++
@@ -1,7 +0,0 @@
-function listOfProducts(productList) {
-    let sortedList = productList.sort();
-    for (let i = 0; i < sortedList.length; i++) {
-        console.log(`${i + 1}.${sortedList[i]}`)
-    }
-}
-listOfProducts(['Watermelon', 'Banana', 'Apples']);
